6ab767900c1e6cf201bda45e03cc6a541a61ce0b
Kirsty Rose <krhome21@gmail.com>
2014-10-27 08:27:53 +0000

    add script

---
+++ try.js
@@ -0,0 +1,59 @@
+$(function(){
+
+	'use strict';
+
+	function runScripts(){
+
+		var setup = {
+
+			init: function(){
+
+				for(var funcs in site_functions) {
+
+					try {
+
+						site_functions[funcs]();
+
+					} catch(error) {
+
+						console.error('[By the way, you broke something! Look in "'+funcs+'" for the error!] - ' + error);
+
+					}
+
+				}
+
+			}
+
+		}
+
+		setup.init();
+
+	}
+	
+	var site_functions  = {
+
+		test: function() {
+
+			var self = this;
+
+			self.init = function() {
+
+				self.method2();
+
+			}
+
+			this.init();
+
+		},
+
+		test2: function() {
+
+			console.log('Sam is a boff');
+
+		}
+
+	}
+
+	runScripts();
+
+});
